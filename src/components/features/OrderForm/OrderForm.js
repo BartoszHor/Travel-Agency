91edd7c4cd0e4e210acb103cacd371ff19c77622
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-10-30 12:53:25 +0100

    Add data fetch to test api

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,10 +4,52 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+import {settings} from '../../../data/settings';
+
+const sendOrder = (options, tripCost, country) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  const tripId = country.trips[0];
+  const tripName = country.name;
+
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripId,
+    tripName,
+    countryCode: country.alpha2Code,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  if(options.name == '' || options.contact == '' || !options.contact.includes('@')) {
+    alert('Please fill in both name and contact with proper format');
+    return;
+  }
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 
 
-const OrderFrom = ({options, tripCost, setOrderOption}) => (
+const OrderFrom = ({options, tripCost, setOrderOption, country}) => (
   <Row>
     {pricing.map(option => (
       <Col md={6} key={option.id}>
@@ -17,6 +59,7 @@ const OrderFrom = ({options, tripCost, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary cost={tripCost}  options={options}/>
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost, country)}>Order now!</Button>
   </Row>
 );
 
@@ -25,6 +68,7 @@ OrderFrom.propTypes = {
   options: PropTypes.object,
   tripCost: PropTypes.string,
   setOrderOption: PropTypes.func,
+  country: PropTypes.object,
 };
 
 export default OrderFrom;
--- src/components/features/OrderOption/OrderOptionText.js
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -3,11 +3,11 @@ import styles from './OrderOption.scss';
 import PropTypes from 'prop-types';
 
 
-const OrderOptionText = ({setOptionValue, currentValue}) => {
+const OrderOptionText = ({setOptionValue, currentValue, inputType}) => {
 
   return (
     <div className={styles.text}>
-      <input type='text'
+      <input type={inputType}
         onChange={event => setOptionValue(event.currentTarget.value)}
         value={currentValue}
       />
@@ -18,6 +18,7 @@ const OrderOptionText = ({setOptionValue, currentValue}) => {
 OrderOptionText.propTypes = {
   setOptionValue: PropTypes.func,
   currentValue: PropTypes.node,
+  inputType: PropTypes.string,
 };
 
 export default OrderOptionText;
